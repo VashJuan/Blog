faccbefd67b2e54c6c1c15da45356d3e2f234a5d
JCornelison <gitKraken@VashonDesign.com>
2024-05-14 18:38:44 -0700

    Updating content

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,10 +1,10 @@
 import { CheckIcon } from '@heroicons/react/20/solid';
 
 const includedFeatures = [
-  'Private forum access',
-  'Member resources',
-  'Entry to annual conference',
-  'Official member t-shirt',
+  'Forum access',
+  'EOC focused resources',
+  'Personalized email support',
+  'Ability to steer direction of future offerings',
 ];
 
 export default function Price() {
@@ -16,19 +16,17 @@ export default function Price() {
             Simple no-tricks pricing
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
-            quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
+            Our software and resources are open-source and freely available,
+            under the permissive MIT License.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
           <div className="p-8 sm:p-10 lg:flex-auto">
             <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-              Lifetime membership
+              Lifetime software
             </h3>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
-              amet indis perferendis blanditiis repellendus etur quidem
-              assumenda.
+              Pay us $0.00 and use (& customize!) the software forever.
             </p>
             <div className="mt-10 flex items-center gap-x-4">
               <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
@@ -59,20 +57,21 @@ export default function Price() {
                 </p>
                 <p className="mt-6 flex items-baseline justify-center gap-x-2">
                   <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    $349
+                    $0.00
                   </span>
                   <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
                     USD
                   </span>
                 </p>
                 <a
-                  href="#"
+                  href="https://github.com/eoconline/"
                   className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get access
                 </a>
                 <p className="mt-6 text-xs leading-5 text-gray-600">
-                  Invoices and receipts available for easy company reimbursement
+                  No need for invoices and receipts. Make things easy for your
+                  budget & finance department.
                 </p>
               </div>
             </div>
